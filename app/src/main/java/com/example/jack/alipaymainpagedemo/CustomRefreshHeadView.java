44926d7737566c6d1ef3aee3c618fc16aa2e8ed9
1363655717 <1363655717@qq.com>
2017-05-19 16:39:51 +0800

    申中佳

--- app/src/main/java/com/example/jack/alipaymainpagedemo/CustomRefreshHeadView.java
+++ app/src/main/java/com/example/jack/alipaymainpagedemo/CustomRefreshHeadView.java
@@ -10,7 +10,7 @@ import com.aspsine.swipetoloadlayout.SwipeTrigger;
 
 /**
  * Created by Administrator on 2017/5/18.
- * 134
+ * 是打发
  */
 
 public class CustomRefreshHeadView extends TextView implements SwipeRefreshTrigger, SwipeTrigger {
